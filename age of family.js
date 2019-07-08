b4a9f4fd5da762236246b702463b5ec8938a09f1
KrisCoop <47994388+KrisCoop@users.noreply.github.com>
2019-07-07 18:16:31 -0600

    a few more done

--- age of family.js
+++ age of family.js
@@ -1,4 +1,43 @@
-// At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
-//
-// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+//  At the annual family gathering, the family likes to find the oldest living family member’s age 
+// and the youngest family member’s age and calculate the difference between them.
+// 
+//  You will be given an array of all the family members' ages, in any order. The ages will be given 
+// in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array 
+// with [youngest age, oldest age, difference between the youngest and oldest age].
 
+let arr1 = [5,6,3,4,2,1,8]
+
+function findAges(x){
+
+    // use sort() to list the array items from smallest to largest and place them in a new variable array.
+    // take the first and last indexes, perhaps by assigning them each a variable.
+    // subtract the smaller index from the larger, and set the result to a 4th variable.
+    // create a new array (5th variable) and compose it thus: [smallest, largest, difference] by inserting the pre-defined 
+    //////  variables in the correct index slots.
+    // return the new variable.
+
+    let newArr = []
+    newArr = x.sort(function(a, b){return a - b});
+    console.log(newArr)
+
+    let small = newArr[0];
+    console.log(small)
+
+    let large = newArr[newArr.length-1]
+    console.log(large)
+
+    let diff = large - small;
+    console.log(diff)
+
+    let ans = [small, large, diff];
+    console.log(ans)
+
+    return ans;
+
+};
+
+console.log(findAges(arr1))
+
+
+// var points = [40, 100, 1, 5, 25, 10];
+// points.sort(function(a, b){return b-a});
--- age predicter.js
+++ age predicter.js
@@ -13,3 +13,40 @@
 
 // Some random tests might fail due to a bug in the JavaScript implementation. Simply resubmit if that happens to you.
 
+
+
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+
+    let agesList = [age1,age2,age3,age4,age5,age6,age7,age8]
+    console.log(agesList)
+
+    agesSquared = []
+    for (var i = 0; i < agesList.length; i++){
+        agesSquared.push(agesList[i] * agesList[i]);
+    }
+
+    console.log(agesSquared)
+
+    let ageSum = agesSquared.reduce((acc, elem) => acc + elem, 0)
+    console.log(ageSum)
+
+    let ageRoot = Math.sqrt(ageSum)
+    console.log(ageRoot)
+
+    let ageRootDiv = ageRoot / 2;
+    console.log(ageRootDiv)
+
+    let ageRound = Math.floor(ageRootDiv);
+    console.log(ageRound)
+
+    return ageRound;
+
+} 
+
+// this long code I wrote passed all the tests on codewars (when I found it). Apparently there is a much shorter solution,
+// which uses Math.hypot() to 'return the square root of the sum of squares of its arguments'. Wow.
+
+
+console.log(predictAge(65,60,75,55,60,63,64,45))
+
+//   Test.assertEquals(predictAge(65,60,75,55,60,63,64,45), 86);
